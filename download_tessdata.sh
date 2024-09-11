6a4e397aae8ed3e7a83226cbb9d1654222008a0c
Cam Feenstra <cameron.l.feenstra@gmail.com>
2024-09-11 11:40:57 -0700

    use fast model

--- download_tessdata.sh
+++ download_tessdata.sh
@@ -8,9 +8,13 @@ TESSDATA_DIR=./build-out/share/tessdata
 
 LANGUAGES="eng"
 
+# tessdata_best can also be used, but it's significantly slower
+# See https://tesseract-ocr.github.io/tessdoc/Data-Files.html
+TESSDATA_REPO=tessdata_fast
+
 cd "$TESSDATA_DIR"
 
 for lang in $LANGUAGES; do
-    echo "Downloading language: $lang"
-    curl -LO --fail "https://raw.githubusercontent.com/tesseract-ocr/tessdata_best/$TESSDATA_VERSION/$lang.traineddata"
+    echo "Downloading language: $TESSDATA_REPO/$lang"
+    curl -LO --fail "https://raw.githubusercontent.com/tesseract-ocr/$TESSDATA_REPO/$TESSDATA_VERSION/$lang.traineddata"
 done
