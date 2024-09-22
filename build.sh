b71940a628c9f7bc4404089449700c6d23ffb4d2
Cam Feenstra <cameron.l.feenstra@gmail.com>
2024-09-22 14:32:04 -0700

    update

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -eo pipefail
+
+mkdir -p build build-out
+
+cd build
+
+CONFIGURE_ARGS="--disable-openmp --disable-doc --disable-graphics"
+if [[ -z "$ENABLE_CPU_OPTIMIZATIONS" ]]; then 
+    CONFIGURE_ARGS+=" --disable-cpu-optimizations"
+fi
+
+case "$(uname -s)" in
+    Linux*) 
+        ../configure \
+            LDFLAGS="-Wl,-rpath,'\$\$ORIGIN/../lib' -Wl,--disable-new-dtags" \
+            $CONFIGURE_ARGS \
+            --prefix=$(realpath ../build-out)
+
+        # make
+        # make install
+        ;;
+    Darwin*)
+        ../configure \
+            $CONFIGURE_ARGS \
+            --prefix=$(realpath ../build-out)
+
+        # make
+        # make install
+
+        # cd ..
+        # ./macos_link.sh
+        # cd -
+        ;;
+    *)
+        echo "Unsupported platform: $(uname -s)"
+        exit 1
+        ;;
+esac
+
+# sed '4i\                                     
+# TESSERACT_DIR=$(dirname $SCRIPT_DIR)
+# ' download_tessdata.sh > build-out/bin/download_tessdata.sh
+# chmod +x build-out/bin/download_tessdata.sh
--- copy_scripts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sed '4i\                                     
-TESSERACT_DIR=$(dirname $SCRIPT_DIR)
-' download_tessdata.sh > build-out/bin/download_tessdata.sh
-chmod +x build-out/bin/download_tessdata.sh
