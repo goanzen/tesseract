e50a52ff432de502878d28813dbd5a83dd528acd
Cam Feenstra <cameron.l.feenstra@gmail.com>
2024-09-22 15:33:34 -0700

    allow neon for arm

--- build.sh
+++ build.sh
@@ -38,7 +38,7 @@ case "$(uname -s)" in
         cd -
 
         cd ..
-        sed '4i\                                     
+        sed '4i\
 TESSERACT_DIR=$(dirname $SCRIPT_DIR)
 ' download_tessdata.sh > build-out/bin/download_tessdata.sh
         chmod +x build-out/bin/download_tessdata.sh
