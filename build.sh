884a8958f424f7f1824f615c01e22dc6a51c2cb5
Cam Feenstra <cameron.l.feenstra@gmail.com>
2024-09-23 09:38:06 -0700

    remove more optimizations

--- build.sh
+++ build.sh
@@ -10,7 +10,9 @@ cd build
 
 CONFIGURE_ARGS="--disable-openmp --disable-doc --disable-graphics"
 if [[ -z "$ENABLE_CPU_OPTIMIZATIONS" ]]; then 
-    CONFIGURE_ARGS+=" --disable-cpu-optimizations"
+    CONFIGURE_ARGS+=" --disable-cpu-optimizations --enable-debug"
+    export CFLAGS="-O1"
+    export CXXFLAGS="-O1"
 fi
 
 export CC=clang
@@ -24,34 +26,34 @@ case "$(uname -s)" in
             $CONFIGURE_ARGS \
             --prefix=$(realpath ../build-out)
 
-        make
-        make install
+        # make
+        # make install
 
-        cp include/config_auto.h ../build-out/
+        # cp include/config_auto.h ../build-out/
 
-        cd ..
-        sed '4i\TESSERACT_DIR=$(dirname $SCRIPT_DIR)' download_tessdata.sh > build-out/bin/download_tessdata.sh
-        chmod +x build-out/bin/download_tessdata.sh
+        # cd ..
+        # sed '4i\TESSERACT_DIR=$(dirname $SCRIPT_DIR)' download_tessdata.sh > build-out/bin/download_tessdata.sh
+        # chmod +x build-out/bin/download_tessdata.sh
         ;;
     Darwin*)
         ../configure \
             $CONFIGURE_ARGS \
             --prefix=$(realpath ../build-out)
 
-        make
-        make install
+#         make
+#         make install
 
-        cp include/config_auto.h ../build-out/
+#         cp include/config_auto.h ../build-out/
 
-        cd ..
-        ./macos_link.sh
-        cd -
+#         cd ..
+#         ./macos_link.sh
+#         cd -
 
-        cd ..
-        sed '4i\
-TESSERACT_DIR=$(dirname $SCRIPT_DIR)
-' download_tessdata.sh > build-out/bin/download_tessdata.sh
-        chmod +x build-out/bin/download_tessdata.sh
+#         cd ..
+#         sed '4i\
+# TESSERACT_DIR=$(dirname $SCRIPT_DIR)
+# ' download_tessdata.sh > build-out/bin/download_tessdata.sh
+#         chmod +x build-out/bin/download_tessdata.sh
         ;;
     *)
         echo "Unsupported platform: $(uname -s)"
