655deef9b2183bb11121ff6bd12393a6f325a720
Cam Feenstra <cameron.l.feenstra@gmail.com>
2024-09-23 09:48:23 -0700

    fix

--- build.sh
+++ build.sh
@@ -26,34 +26,34 @@ case "$(uname -s)" in
             $CONFIGURE_ARGS \
             --prefix=$(realpath ../build-out)
 
-        # make
-        # make install
+        make
+        make install
 
-        # cp include/config_auto.h ../build-out/
+        cp include/config_auto.h ../build-out/
 
-        # cd ..
-        # sed '4i\TESSERACT_DIR=$(dirname $SCRIPT_DIR)' download_tessdata.sh > build-out/bin/download_tessdata.sh
-        # chmod +x build-out/bin/download_tessdata.sh
+        cd ..
+        sed '4i\TESSERACT_DIR=$(dirname $SCRIPT_DIR)' download_tessdata.sh > build-out/bin/download_tessdata.sh
+        chmod +x build-out/bin/download_tessdata.sh
         ;;
     Darwin*)
         ../configure \
             $CONFIGURE_ARGS \
             --prefix=$(realpath ../build-out)
 
-#         make
-#         make install
+        make
+        make install
 
-#         cp include/config_auto.h ../build-out/
+        cp include/config_auto.h ../build-out/
 
-#         cd ..
-#         ./macos_link.sh
-#         cd -
+        cd ..
+        ./macos_link.sh
+        cd -
 
-#         cd ..
-#         sed '4i\
-# TESSERACT_DIR=$(dirname $SCRIPT_DIR)
-# ' download_tessdata.sh > build-out/bin/download_tessdata.sh
-#         chmod +x build-out/bin/download_tessdata.sh
+        cd ..
+        sed '4i\
+TESSERACT_DIR=$(dirname $SCRIPT_DIR)
+' download_tessdata.sh > build-out/bin/download_tessdata.sh
+        chmod +x build-out/bin/download_tessdata.sh
         ;;
     *)
         echo "Unsupported platform: $(uname -s)"
